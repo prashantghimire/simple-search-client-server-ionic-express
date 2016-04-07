cdd383006039b505e3760603c889c60720b43afb
Prashant Ghimire <pghimire@vermasystems.com>
2016-04-07 16:33:04 -0500

    basic search implementation

--- client/srdclient/www/js/controllers.js
+++ client/srdclient/www/js/controllers.js
@@ -2,6 +2,37 @@ angular.module('srd.controllers', [])
     .controller('AppCtrl',['$scope', function ($scope) {
 
     }])
-    .controller('HomeCtrl', ['$scope', function ($scope) {
-        $scope.msg = "Hello World";
-    }]);
+    .controller('HomeCtrl', ['$scope', '$http','$q', function ($scope, $http, $q) {
+
+        var results = [
+            "Apple",
+            "Banana",
+            "Broccoli",
+            "Cabbage",
+            "Cauliflower",
+            "Drinks",
+            "Eggs",
+            "Fruits"
+        ];
+
+        $scope.list = [];
+
+        $scope.filter = function (searchKey) {
+            $scope.list = [];
+            searchKey = String(searchKey).toLowerCase();
+            if(!searchKey){
+                console.log("No input");
+                return;
+            }
+            results.forEach(function (item) {
+                item = String(item);
+                if(item.toLowerCase().indexOf(searchKey) != -1){
+                    console.log(item);
+                    $scope.list.push(item);
+                }
+            });
+        }
+    }])
+    .service('API', function ($http) {
+
+    });
