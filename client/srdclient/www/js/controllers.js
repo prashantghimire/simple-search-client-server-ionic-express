767b5eab4eaa69ddb566c50d03dd68d2bcc70973
Prashant Ghimire <pghimire@vermasystems.com>
2016-04-07 17:32:55 -0500

    added feature localstorage to save http request.

--- client/srdclient/www/js/controllers.js
+++ client/srdclient/www/js/controllers.js
@@ -2,37 +2,43 @@ angular.module('srd.controllers', [])
     .controller('AppCtrl',['$scope', function ($scope) {
 
     }])
-    .controller('HomeCtrl', ['$scope', '$http','$q', function ($scope, $http, $q) {
+    .controller('HomeCtrl', ['$scope', 'API',function ($scope, API) {
 
-        var results = [
-            "Apple",
-            "Banana",
-            "Broccoli",
-            "Cabbage",
-            "Cauliflower",
-            "Drinks",
-            "Eggs",
-            "Fruits"
-        ];
+        var localStorage = window.localStorage;
+        var data = localStorage.getItem("srddb");
+        var exists = (data !== null);
+        if(!exists){
+            API
+                .get('http://jsonplaceholder.typicode.com/posts')
+                .then(function(response){
+                    var results = JSON.stringify(response.data);
+                    localStorage.setItem("srddb",results);
+                });
+        }
+        var results = JSON.parse(localStorage.getItem("srddb"));
 
         $scope.list = [];
-
         $scope.filter = function (searchKey) {
             $scope.list = [];
             searchKey = String(searchKey).toLowerCase();
             if(!searchKey){
-                //$scope.list.push("No input");
                 return;
             }
             results.forEach(function (item) {
-                item = String(item);
-                if(item.toLowerCase().indexOf(searchKey) != -1){
-                    console.log(item);
-                    $scope.list.push(item);
+                if(item['title'].toLowerCase().indexOf(searchKey) > -1){
+                    $scope.list.push(item.title);
                 }
             });
         }
+
+
     }])
     .service('API', function ($http) {
+        var get = function (url) {
+            return $http.get(url);
+        };
 
+        return {
+            get: get
+        }
     });
