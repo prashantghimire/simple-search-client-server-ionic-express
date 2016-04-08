35c0f4077de1324959e8db5132cd2055c6bf58c5
Prashant Ghimire <pghimire@vermasystems.com>
2016-04-07 20:12:53 -0500

    Bugfix wth localstorage.

--- client/srdclient/www/js/controllers.js
+++ client/srdclient/www/js/controllers.js
@@ -4,41 +4,55 @@ angular.module('srd.controllers', [])
     }])
     .controller('HomeCtrl', ['$scope', 'API',function ($scope, API) {
 
-        var localStorage = window.localStorage;
-        var data = localStorage.getItem("srddb");
-        var exists = (data !== null);
-        if(!exists){
-            API
-                .get('http://jsonplaceholder.typicode.com/posts')
-                .then(function(response){
-                    var results = JSON.stringify(response.data);
-                    localStorage.setItem("srddb",results);
-                });
-        }
-        var results = JSON.parse(localStorage.getItem("srddb"));
+        $scope.searchBy = 'title';
 
         $scope.list = [];
-        $scope.filter = function (searchKey) {
-            $scope.list = [];
-            searchKey = String(searchKey).toLowerCase();
-            if(!searchKey){
-                return;
-            }
-            results.forEach(function (item) {
-                if(item['title'].toLowerCase().indexOf(searchKey) > -1){
-                    $scope.list.push(item.title);
+
+        API.get().then(function (results) {
+            $scope.filter = function (searchKey) {
+                $scope.list = [];
+                searchKey = String(searchKey).toLowerCase();
+                if(!searchKey){
+                    return;
                 }
-            });
-        }
-
-
+                results.forEach(function (item) {
+                    if(item[$scope.searchBy].toLowerCase().indexOf(searchKey) > -1){
+                        $scope.list.push(item[$scope.searchBy]);
+                    }
+                });
+            }
+        }, function (err) {
+            alert("Sorry! Error occurred while searching.");
+        });
     }])
-    .service('API', function ($http) {
-        var get = function (url) {
-            return $http.get(url);
+    .service('API', function ($http, $q) {
+
+        var get = function () {
+
+            var localStorageKey = "srddb";
+            var deferred = $q.defer();
+            var localStorage = window.localStorage;
+            var data = localStorage.getItem(localStorageKey);
+
+            var exists = (data !== null);
+
+            if(!exists){
+                $http
+                    .get('http://jsonplaceholder.typicode.com/posts')
+                    .then(function(response){
+                        localStorage.setItem(localStorageKey,JSON.stringify(response.data));
+                        deferred.resolve(response.data);
+                    }, function (err) {
+                        deferred.reject({"error": err});
+                    });
+            } else {
+                deferred.resolve(JSON.parse(localStorage.getItem(localStorageKey)));
+            }
+            return deferred.promise;
         };
 
         return {
             get: get
         }
+
     });
