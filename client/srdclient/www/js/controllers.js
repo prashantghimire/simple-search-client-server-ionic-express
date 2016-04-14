1bb08f4d120d646722fab4a430759b74b7722de9
Prashant Ghimire <pghimire@vermasystems.com>
2016-04-13 23:21:06 -0500

    search by different field possible

--- client/srdclient/www/js/controllers.js
+++ client/srdclient/www/js/controllers.js
@@ -1,3 +1,4 @@
+
 angular.module('srd.controllers', ['ion-sticky'])
 
     .controller('AppCtrl',['$scope','$state','$rootScope','API','Constant', function ($scope, $state, $rootScope, API, Constant) {
@@ -18,13 +19,14 @@ angular.module('srd.controllers', ['ion-sticky'])
         }
     }])
 
-    .controller('HomeCtrl', ['$scope','$rootScope','API',function ($scope, $rootScope, API) {
+    .controller('HomeCtrl', ['$scope','$rootScope','API', 'Utils', function ($scope, $rootScope, API, Utils) {
 
-        $scope.searchBy = 'title';
 
         $scope.list = [];
 
         API.get().then(function (results) {
+
+            $scope.searchBy = Utils.getDefaultSearchBy(results);
 
             $scope.filter = function (searchKey) {
 
@@ -32,21 +34,21 @@ angular.module('srd.controllers', ['ion-sticky'])
 
                 searchKey = String(searchKey).toLowerCase();
 
-                if(!searchKey){
-                    return;
-                }
+                if(!searchKey) return;
 
                 if($rootScope.updated) {
                     results = API.getLocalData();
                     $rootScope.updated = false;
                 }
-
-                console.log("number of results", results.length);
-
                 results.forEach(function (item) {
-                    if(item[$scope.searchBy].toLowerCase().indexOf(searchKey) > -1){
-                        $scope.list.push(item);
+
+                    var searchByValue = item[$scope.searchBy].value;
+
+                    if(searchByValue.toLowerCase().indexOf(searchKey) > -1){
+                        var view_item = {"value": searchByValue, "key": item.id};
+                        $scope.list.push(view_item);
                     }
+
                 });
             };
         }, function (err) {
@@ -96,9 +98,27 @@ angular.module('srd.controllers', ['ion-sticky'])
         }
 
     }])
+    .service('Utils',[function () {
+        var getDefaultSearchBy = function (results) {
 
+            for(var i = 0 ; i < results.length; i++){
+                var item = results[i];
+                for(var key in item){
+                    var data_type = item[key].data_type || "";
+                    if(data_type.indexOf("name") > -1){
+                        return key;
+                    }
+                }
+            }
+        };
+
+        return {
+            getDefaultSearchBy: getDefaultSearchBy
+        }
+
+    }])
     .constant('Constant',
         {
             'local_storage_key':'srddb',
-            'api_url':'http://jsonplaceholder.typicode.com/posts'
+            'api_url':'js/sample.json'
         });
